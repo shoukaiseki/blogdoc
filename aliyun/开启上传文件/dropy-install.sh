6e09ac27884f735404b93de5453c8fb79292940f
shoukaiseki <jiang28555@gmail.com>
2021-01-22 18:26:20 +0800

    ch

--- aliyun/开启上传文件/dropy-install.sh
+++ aliyun/开启上传文件/dropy-install.sh
@@ -22,7 +22,7 @@ set -o nounset                              # Treat unset variables as an error
 
 cd /tmp
 wget https://gitee.com/shoukaiseki/blogdoc/raw/master/aliyun/%E5%BC%80%E5%90%AF%E4%B8%8A%E4%BC%A0%E6%96%87%E4%BB%B6/droopy8123 -O droopy8123
-cp -f /tmp/droopy8123  /data/server/droopy
+cp -f /tmp/droopy8123  /data/server/droopy8123
 
 wget https://gitee.com/shoukaiseki/blogdoc/raw/master/aliyun/%E5%BC%80%E5%90%AF%E4%B8%8A%E4%BC%A0%E6%96%87%E4%BB%B6/open_upload_http.sh -O open_upload_http.sh
 chmod +x open_upload_http.sh
