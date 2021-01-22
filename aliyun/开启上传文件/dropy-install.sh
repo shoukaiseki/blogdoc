36d7c7fe83e8c72b1221a2f1cfcdd14d5d5bce14
shoukaiseki <jiang28555@gmail.com>
2021-01-22 18:21:50 +0800

    ch

--- aliyun/开启上传文件/dropy-install.sh
+++ aliyun/开启上传文件/dropy-install.sh
@@ -25,6 +25,7 @@ wget https://gitee.com/shoukaiseki/blogdoc/raw/master/aliyun/%E5%BC%80%E5%90%AF%
 cp -f /tmp/droopy8123  /data/server/droopy
 
 wget https://gitee.com/shoukaiseki/blogdoc/raw/master/aliyun/%E5%BC%80%E5%90%AF%E4%B8%8A%E4%BC%A0%E6%96%87%E4%BB%B6/open_upload_http.sh
-cp -f /tmp/open_upload_http.sh  /bin/open_upload_http.sh
 chmod +x open_upload_http.sh
+cp -f /tmp/open_upload_http.sh  /bin/open_upload_http.sh
+chmod +x /bin/open_upload_http.sh
 
