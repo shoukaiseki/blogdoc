8a1e260bcd0c1e4042f67eadc03d4f24493b4aaa
shoukaiseki <jiang28555@gmail.com>
2021-01-29 00:46:13 +0800

    ch

--- aliyun/服务部署/opensuse/runspringboot.sh
+++ aliyun/服务部署/opensuse/runspringboot.sh
@@ -2,6 +2,7 @@
 
 APP_HOME=/data/server/springboot/wb520
 AppName=wb520-admin.jar
+CATALINA_TMPDIR=$APP_HOME/temp
 
 #JVM参数
 JVM_OPTS="-Dname=$AppName -Duser.home=$APP_HOME -Duser.timezone=Asia/Shanghai -Xms512M -Xmx2048M -XX:+HeapDumpOnOutOfMemoryError  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
