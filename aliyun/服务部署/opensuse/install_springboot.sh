4bc542890620205a9a3bc689d71ad0d830520043
shoukaiseki <jiang28555@gmail.com>
2021-01-30 16:14:55 +0800

    ch

--- aliyun/服务部署/opensuse/install_springboot.sh
+++ aliyun/服务部署/opensuse/install_springboot.sh
@@ -19,6 +19,13 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+if [ ! -n "$1" ]; then
+  echo "$1 is empty"
+  exit 1
+fi
+
+
+
 zypper install ranger -y
 
 mkdir /data
@@ -30,6 +37,8 @@ mkdir /data/server/springboot/wb000/logs
 mkdir /data/server/springboot/wb000/templogs
 mkdir /data/server/springboot/wb000/temp
 
+
+
 groupadd springbootgroup
 useradd springboot -G springbootgroup
 
@@ -38,12 +47,23 @@ chown -R springboot.springbootgroup springboot
 mkdir /home/springboot
 chown -R springboot.springbootgroup /home/springboot
 
-mkdir /data/usr
-cd /data/usr
-wget http://asus88.vicp.net:9000/jdk/jdk-11.0.9_linux-x64_bin.tar.gz
-tar zxvf jdk-11.0.9_linux-x64_bin.tar.gz
+#如果文件夹不存在，下载jdk解压
+if [ ! -d "/data/usr/jdk-11.0.9" ]; then
+	mkdir /data/usr
+	cd /data/usr
+	wget http://asus88.vicp.net:9000/jdk/jdk-11.0.9_linux-x64_bin.tar.gz
+	tar zxvf jdk-11.0.9_linux-x64_bin.tar.gz
+fi
 
+wget https://gitee.com/shoukaiseki/blogdoc/raw/master/aliyun/%E6%9C%8D%E5%8A%A1%E9%83%A8%E7%BD%B2/opensuse/wb000.service
+wget https://gitee.com/shoukaiseki/blogdoc/raw/master/aliyun/%E6%9C%8D%E5%8A%A1%E9%83%A8%E7%BD%B2/opensuse/stopspringboot.sh
+wget https://gitee.com/shoukaiseki/blogdoc/raw/master/aliyun/%E6%9C%8D%E5%8A%A1%E9%83%A8%E7%BD%B2/opensuse/servicerun.sh
+wget https://gitee.com/shoukaiseki/blogdoc/raw/master/aliyun/%E6%9C%8D%E5%8A%A1%E9%83%A8%E7%BD%B2/opensuse/runspringboot.sh
 
-
+sed -i 's/wb000/wb001/g' stopspringboot.sh
 
 echo '如果systemctl方式起不了服务,则将 /data/server/springboot 777 授权'
+
+
+
+
