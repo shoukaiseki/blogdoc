19ca9a93cb837767d4bc20a3e659854174764d61
shoukaiseki <jiang28555@gmail.com>
2021-02-02 12:01:48 +0800

    ch

--- aliyun/服务部署/opensuse/runspringboot.sh
+++ aliyun/服务部署/opensuse/runspringboot.sh
@@ -5,7 +5,8 @@ AppName=wb000-admin.jar
 CATALINA_TMPDIR=$APP_HOME/temp
 
 #JVM参数
-JVM_OPTS="-Dname=$AppName -Duser.home=$APP_HOME -Duser.timezone=Asia/Shanghai -Xms512M -Xmx2048M -XX:+HeapDumpOnOutOfMemoryError  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
+#JVM_OPTS="-Dname=$AppName -Duser.home=$APP_HOME -Duser.timezone=Asia/Shanghai -Xms512M -Xmx2048M -XX:+HeapDumpOnOutOfMemoryError  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
+JVM_OPTS="-Dname=$AppName -Duser.home=$APP_HOME -Duser.timezone=Asia/Shanghai  -XX:+HeapDumpOnOutOfMemoryError  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
 SPRING_OPTS=" -Dlogging.path=$APP_HOME/templogs/  --spring.config.location=$APP_HOME/application.yml"
 LOG_PATH=$APP_HOME/logs/$AppName.log
 
